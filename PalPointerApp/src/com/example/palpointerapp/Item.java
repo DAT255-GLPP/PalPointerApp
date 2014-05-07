c4ee880132051e7adb1317a99330fa4401b57eda
Ludvig Börjesson <bludvig@student.chalmers.se>
2014-05-07 15:10:03 +0200

    Microsoft-support

---
+++ PalPointerApp/src/com/example/palpointerapp/Item.java
@@ -0,0 +1,11 @@
+package com.example.palpointerapp;
+
+public class Item {
+    public String Id;
+    public String Text;
+    
+    
+    public void setText (String text){
+    	this.Text = text;
+    }
+}
